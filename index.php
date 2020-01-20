34c9ac7c3bc9f2bd29294ede230638d1bd1bb513
robertrakcharoen <48210880+robertrakcharoen@users.noreply.github.com>
2020-01-20 15:38:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -66,6 +66,16 @@ require('query.php')
                     } else {
                         echo "0 results";
                     }
+
+                    $conn->close();
+                    function limit_text($text, $limit) {
+                        if (str_word_count($text, 0) > $limit) {
+                            $words = str_word_count($text, 2);
+                            $pos = array_keys($words);
+                            $text = substr($text, 0, $pos[$limit]) . '...';
+                        }
+                        return $text;
+                    }
                    ?>
                 </tbody>
               </table><br>
