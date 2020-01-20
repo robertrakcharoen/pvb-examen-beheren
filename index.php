fc8dde71c7f5655db6d7db29e3894f9e12de3a10
robertrakcharoen <48210880+robertrakcharoen@users.noreply.github.com>
2020-01-20 13:43:58 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Restaurant</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- bootstrap css - gebruikt voor responsive maken -->
+    <script src="https://kit.fontawesome.com/9e77f6e4c2.js" crossorigin="anonymous"></script>
+  </head>
+  <body id="index">
+    <div class="limiter">
+    <div class="container-table100">
+        <div class="wrap-table100">
+            <div class="table100">
+
+
+            </div>
+      </div>
+  </div>
+</div>
+
+  </body>
+</html>
