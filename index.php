4bd438294306b13deacfbcfbe8fae8faf65ca990
robertrakcharoen <48210880+robertrakcharoen@users.noreply.github.com>
2020-01-20 15:37:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -44,6 +44,28 @@ require('query.php')
                     }
 
                     $sql = "SELECT * FROM `restaurants_info` WHERE id BETWEEN '$begin' AND '$eind'";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        // output data of each row
+                        while($row = $result->fetch_assoc()) {
+                            echo "<tr>
+                    <td style='width: 2px' class='column1'>". $row["id"]."</td>
+                    <td class='column5'><a href='". $row["website_naam"]."' target='_blank'><i class=\"fas fa-globe-americas\"></i></a></td>
+                    <td class='column3'><a target='_blank' href='/codes/menu.php?page=".$row['id']."'>". $row["restaurant_naam"]. "</a></td>
+                    <td class='column2'>". $row["Ingevoerd_door"]. "</td>
+                    <td class='column2'>". $row["Status"]. "</td>
+                    <td title='". $row["Opmerking"] . "' class='column2'>". limit_text($row["Opmerking"], 4) . "</td>
+                    <td class='column2'>". $row["Controlle_Door"]. "</td>
+                    <td class='column2'>". $row["Status_2"]. "</td>
+                    <td title='". $row["Opmerking_2"]. "' class='column2'>". limit_text($row["Opmerking_2"], 4) . "</td>
+                    <td style='width: 5px;' class='column6'><a href='". $row["Nieuw_URL"]."' target='_blank'><i class=\"fas fa-file-image\"></i></a> - <a href='https://demo-eten.nl/". $row["restaurant_url"].".html' target='_blank'><i class=\"fas fa-globe-americas\"></i></a></td>
+              </tr>
+
+              "; }
+                    } else {
+                        echo "0 results";
+                    }
                    ?>
                 </tbody>
               </table><br>
