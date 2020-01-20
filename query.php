7bc9b7df48824e4e7bec2cf795f2346b0ebffc72
robertrakcharoen <48210880+robertrakcharoen@users.noreply.github.com>
2020-01-20 13:03:00 +0100

    Update query.php

---
+++ query.php
@@ -0,0 +1,10 @@
+<?php
+  $servername = "localhost";
+  $username = "rubix03_restaurants";
+  $password = "Lekkerpizz@3";
+  $dbname = "rubix03_restaurants";
+
+
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+ ?>
