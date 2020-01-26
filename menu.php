b3d69cdbd94f5c1b6d52eccd76ff52c3130572c0
robertrakcharoen <48210880+robertrakcharoen@users.noreply.github.com>
2020-01-26 19:10:17 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,3 +1,12 @@
 <?php
 include 'query.php';
  ?>
+ <?php
+$results_per_page = 225;
+if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+$start_from = ($page-1) * $results_per_page;
+$restaurant = $_GET["page"];
+$sql = "SELECT * FROM restaurants_info WHERE id = "." $restaurant ";
+$result = $conn->query($sql);
+
+?>
